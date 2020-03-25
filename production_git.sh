6ea33e53dacaa328fa4219893c77fdd1fd1f4bf0
sd3428783 <419600150@qq.com>
2020-03-25 13:37:18 +0800

    Create production_git.sh

---
+++ production_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ilex
+echo ${BUILD_TIMESTAMP}
+if [ $GIT_PREVIOUS_SUCCESSFUL_COMMIT == $GIT_COMMIT ];then
+        echo "no change,skip build";
+        exit 0
+else
+		echo "git pull commit id not equals to current commit id trigger build"
+		/usr/local/node/bin/cnpm install
+		echo $NODE_ENV
+		/usr/local/node/bin/npm run build
+fi
